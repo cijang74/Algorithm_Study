8265476a03f3e093b254f649b97e7305096d78ee
Jaehoon Kim <cijang74@naver.com>
2026-01-20 23:44:41 +0900

    [level 2] Title: 소수 찾기, Time: 0.82 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 제곱근 판별 함수
+bool checkPrimeNum(int n)
+{
+    // 0과 1 제외
+    if(n == 0 || n == 1)
+    {
+        return false;
+    }
+
+    // 나눠지는 수가 있는지 제곱근까지 확인
+    for(int i = 2; i < (int)sqrt(n) + 1; i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int solution(string numbers) 
+{
+    int answer = 0;
+
+    // 11과 011같은 중복값 피하기 위해 Set 사용
+    set<int> numSet;
+
+    // next_permutation는 사전순으로 작동하므로 정렬된 상태에서 해야 함
+    sort(numbers.begin(), numbers.end());
+
+    int r = 0;
+    int size = numbers.length();
+
+    // 모든 자리수의 순열 구하기 (이캠퍼스 블로그 참고)
+    while(r <= size)
+    {
+        do 
+        {
+            string s = "";
+            for (int i = 0; i < r; i++)
+            {
+                s += numbers[i];
+            }
+
+            if(s != "") // 맨 처음 r = 0일 때 ""를 stoi하지 않도록 제어
+            {
+                numSet.insert(stoi(s));
+            }
+
+            reverse(numbers.begin() + r, numbers.end()); // 중복제거
+        } while(next_permutation(numbers.begin(), numbers.end()));
+        r++;
+    }
+
+    for(int i : numSet)
+    {
+        if(checkPrimeNum(i))
+        {
+            answer++;
+        }
+    }
+
+    return answer;
+}
